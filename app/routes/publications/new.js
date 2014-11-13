5570a3cb213ef639063e949caa731883f5f3ad94
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2014-11-13 15:06:53 +0100

    minor errors

--- app/routes/publications/new.js
+++ app/routes/publications/new.js
@@ -9,13 +9,13 @@ export default Ember.Route.extend({
       var that = this;
       var successHandler = function(model) {
         that.transitionTo('publications.show', model.id);
-      }
+      };
       var errorHandler = function(reason) {
         console.log(reason);
         that.controller.set('hasErrors', true);
-        that.controller.set('errors', reason.responseJSON.errors)
+        that.controller.set('errors', reason.responseJSON.errors);
         return false;
-      }
+      };
       this.store.save('publication',model).then(successHandler, errorHandler);
     }
   }
