f870cacf69a351d683156143f6f4880c82defa4d
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2014-12-05 12:21:54 +0100

    publications/manage appearence

--- app/routes/publications/manage/index.js
+++ app/routes/publications/manage/index.js
@@ -2,6 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions:{
+    create: function(model) {
+      if(confirm('Create, alltså')) {
+        var that = this;
+        var successHandler = function(model) {
+        that.store.find('draft').then(function(drafts) {
+          that.controllerFor('publications.manage').set('model',drafts);
+          // här väljer man att gå till den första posten om listan är icke-tom
+          that.transitionTo('publications.manage.show.edit', model);
+        });        
+      };
+      var errorHandler = function(reason) {
+        console.log(reason);
+        that.controller.set('hasErrors', true);
+        that.controller.set('showErrorHeader', true);
+        that.controller.set('errors', reason.responseJSON.errors);
+        return false;
+      };
+        this.store.save('publication', {}, {"datasource": 'none'}).then(successHandler, errorHandler);
+      };
+    },
     import: function(sourceData){
       var that = this;
       var successHandler = function(model) {
