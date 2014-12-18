b216890d4b85b56b551eb7fb800bd1262c398763
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2014-12-18 16:07:48 +0100

    file upload button

--- app/controllers/publications/manage/index.js
+++ app/controllers/publications/manage/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    laddaupp: function() {
+      Ember.$('#fulgrej').click();
+    }
+  },
   needs: ['publications/manage'],
   sources: [{value:'pubmed', label:'PubMed'},
             {value:'gupea',  label:'GUPEA'},
