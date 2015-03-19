21595627108ece6ea1ba767dc5b814f90d82a4ff
Per Larsson <per.larsson@ub.gu.se>
2015-03-19 10:46:18 +0100

    Creating publications from file is now working.

--- app/components/file-uploader.js
+++ app/components/file-uploader.js
@@ -2,31 +2,45 @@ import Ember from 'ember';
 import ENV from 'gup/config/environment';
 export default Ember.FileField.extend({
   filesDidChange: (function() {
-    console.log('HÄJ');
     var controller = this.get('targetObject');
     var that=this;
     var successHandler = function() {
       controller.set('hasMesg', true);
       controller.set('showMesgHeader', true);
-      controller.set('mesg', 'Det blev rätt');
-      that.triggerAction({action: 'refreshLists'});
+      controller.set('mesg', 'Success');
       that.set('value', '');
       controller.transitionToRoute('publications.manage');
+      preFilters.remove(authPrefilter);
     };
+
     var errorHandler = function(reason) {
       console.log(reason);
       controller.set('hasErrors', true);
       controller.set('showMesgHeader', true);
       controller.set('errors', reason.responseJSON.errors);
+      preFilters.remove(authPrefilter);
       return false;
     };
+
     var files = this.get('files');
+    var token = this.container.lookup('simple-auth-session:main').get('token');
+    var preFilters = Ember.$.Callbacks();
+    
+    var authPrefilter = function(options) {
+      if(!options.headers) {
+        options.headers = {};
+      }
+      options.headers['Authorization'] = "Token "+token;
+      return options;
+    };
 
     var uploader = Ember.Uploader.create({
-      url: ENV.APP.serviceURL + '/publications/import_file'
+      url: ENV.APP.serviceURL + '/publications'
     });
 
     if (!Ember.isEmpty(files)) {
+      Ember.$.ajaxPrefilter(preFilters.fire);
+      preFilters.add(authPrefilter);      
       uploader.upload(files[0]).then (successHandler, errorHandler);
     }
   }).observes('files')
