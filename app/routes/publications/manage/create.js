6882f85f7f3eb9c656f79da7345ba4043e59c909
Magnus Kull <magnus.kull@ub.gu.se>
2015-06-02 18:32:28 +0200

    reset import data etc on setup controller for create route

--- app/routes/publications/manage/create.js
+++ app/routes/publications/manage/create.js
@@ -3,5 +3,14 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
+  setupController: function(controller) {
+
+    controller.set('selectedSource', null);
+    controller.set('sourceId', null);
+    controller.set('importData', null);
+    controller.set('error', null);      
+
+  }
+
 
 });
