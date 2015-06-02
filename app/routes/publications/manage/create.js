f902b393d5ef5b4c3f9eef5e3e21f28e58018aa7
Magnus Kull <magnus.kull@ub.gu.se>
2015-06-02 15:33:51 +0200

    created new import route and changes to adapter

--- app/routes/publications/manage/create.js
+++ app/routes/publications/manage/create.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function()
-  {
-    return this.store.find('publication_type');
-  }
+
+
 });
