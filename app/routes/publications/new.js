d3ecc987e3880ddb70b1bb0033f5e3fd374842fa
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-06-03 13:13:26 +0200

    Fix so that errors show up when trying to publish an invalid publication

--- app/routes/publications/new.js
+++ app/routes/publications/new.js
@@ -440,7 +440,7 @@ export default Ember.Route.extend({
         var errorHandler = function(reason) {
             that.controller.set('hasErrors', true);
             that.controller.set('showMesgHeader', true);
-            that.controller.set('errors', reason.responseJSON.errors);
+            that.controller.set('errors', reason.error.errors);
             Ember.$("body").removeClass("loading");
             return false;
         };
