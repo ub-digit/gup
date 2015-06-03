3f148b7ec12b03e4b17227fb3f40fa56ca45b4f4
johan larsson <johan.larsson@ub.gu.se>
2015-06-03 10:33:39 +0200

    removed route

--- app/controllers/publications/manage/publication-type-picker.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-	selectedPublicationType: null,
-
-	selectedPublicationTypeChanged: function() {
-		alert("changed");
-	}.observes('selectedPublicationType')
-});
