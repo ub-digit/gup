e5ff23d837bb90efb37486ca2a5d23c94a05085c
johan larsson <johan.larsson@ub.gu.se>
2015-06-03 10:08:08 +0200

    remove code

--- app/controllers/publications/manage/index.js
+++ app/controllers/publications/manage/index.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    uploadFile: function() {
-      Ember.$('#fileUploader').click();
-    }
-  },
-  needs: ['publications/manage'],
-  sources: [{value:'pubmed', label:'PubMed'},
-            {value:'gupea',  label:'GUPEA'},
-            {value:'scopus', label:'Scopus'},
-            {value:'libris', label:'Libris'}],
-  sourceData: {}
 });
