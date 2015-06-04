14115f741ec924659acfbdff84732d239af45050
johan larsson <johan.larsson@ub.gu.se>
2015-06-04 14:09:12 +0200

    simple confirm added for delete

--- app/controllers/publications/dashboard/drafts.js
+++ app/controllers/publications/dashboard/drafts.js
@@ -2,27 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  deletePublication: function(id) {
+    var that = this;
+    this.store.destroy('publication', id).then(function() {
+      that.send('refreshModel');
+    });
+  },
   actions: {
-
     delete: function(id) {
-
-      var that = this;
-
-      this.store.destroy('publication', id).then(function() {
-
-        that.send('refreshModel');
-
-      },
-      function() {
-
-        
-
-      }
-
-    );
-
-
+      var txt;
+      var r = confirm("Är du verkligen riktigt säker på detta?");
+      if (r == true) {
+        this.deletePublication(id);
+      } 
+    }
   }
-
-}
 });
