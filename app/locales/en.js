23707c216586635ef893aa84ec95a1c945e1d505
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-06-05 08:56:14 +0200

    Added some language strings.

--- app/locales/en.js
+++ app/locales/en.js
@@ -3,11 +3,30 @@ export default {
 	mainMenu: {
 		applicationName: 'GUP 3',
 		lang: 'Swedish',
-		publications: 'Publications'
+		publications: 'Publications',
+    logOut: 'Log out'
 	},
 	login: {
 		xaccount: 'x-account',
 		password: 'Password',
 		button: 'Log in'
-	}
+	},
+  publication: {
+    labels: {
+      publications: 'Publications',
+      createdAt: 'Created',
+      updatedAt: 'Updated',
+      publishedAt: 'Published'
+    }
+  },
+  text: {
+    youHaveNoDrafts: 'You have no drafts.',
+    drafts: 'Drafts',
+    published: 'Published',
+    registeredOrEditedByMe: 'Registered/edited by me',
+    youHaveNoRegisteredOrEditedPublications: 'You have no registered or edited publications.'
+  },
+  labels: {
+    registerNew: 'Register New'
+  }
 };
--- app/locales/sv.js
+++ app/locales/sv.js
@@ -3,11 +3,30 @@ export default {
 	mainMenu: {
 		applicationName: 'GUP 3',
 		lang: 'Engelska',
-		publications: 'Publikationer'
+		publications: 'Publikationer',
+    logOut: 'Logga ut'
 	},
 	login: {
 		xaccount: 'x-konto',
 		password: 'Lösenord',
 		button: 'Logga in'
-	}
+	},
+  publication: {
+    labels: {
+      publications: 'Publikationer',
+      createdAt: 'Skapad',
+      updatedAt: 'Uppdaterad',
+      publishedAt: 'Publicerad'
+    }
+  },
+  text: {
+    youHaveNoDrafts: 'Du har inga utkast.',
+    drafts: 'Utkast',
+    published: 'Publicerade',
+    registeredOrEditedByMe: 'Registrerade/redigerade av mig',
+    youHaveNoRegisteredOrEditedPublications: 'Du har inga registrerat eller redigerat några publikationer.'
+  },
+  labels: {
+    registerNew: 'Registrera ny'
+  }
 };
