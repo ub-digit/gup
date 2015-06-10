176df5605441f21e73e407095e356c55338ac20c
Magnus Kull <magnus.kull@ub.gu.se>
2015-06-10 16:33:25 +0200

    enhancements of categories component

--- app/components/category-item.js
+++ app/components/category-item.js
@@ -1,22 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  MAX_NUMBER_OF_CATEGORIES: 3,
+
   expanded: false,
   addSelectedCategoryString: 'addSelectedCategory',
   isExpanded: Ember.computed.equal('expanded', true),
+
+  isSelected: Ember.computed('categoryList.@each', 'category.svepid', function() {
+
+    return !(this.get('categoryList').indexOf(this.get('category.svepid')) === -1);
+
+  }),
 
   actions: {
     toggle: function() {
       this.toggleProperty('expanded');
     },
-    setCategory: function() {
-      var that = this;
-      var id = this.get('category.svepid');
-      // Check if category already exists in list, and that maximum value is not reached
-      if (that.get('categoryList').indexOf(id) === -1 && that.get('categoryList.length') < that.get('MAX_NUMBER_OF_CATEGORIES')){
-        that.get('categoryList').pushObject(that.get('category.svepid'));
-      }
+
+    addSelectedCategory: function(id) {
+
+      this.sendAction('addSelectedCategoryString', id);
+
     }
   }
 });
