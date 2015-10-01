485e5a1e849a51e1729a51f2b0f067edd526f725
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-10-01 12:06:55 +0200

    Add dl elements for show index page for better readability

--- app/components/field-component.js
+++ app/components/field-component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName:'',
   // Determines if input or presentation template should be used
   isPresentation: Ember.computed.equal('format', 'presentation'),
   isEditable: Ember.computed('isPresentation', function(){
