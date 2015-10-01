e0a80a09a8e76358a17a8c16f7d9ac86e804d8b0
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-10-01 12:36:51 +0200

    Fix bug where year selections get recreated each time transition is made

--- app/controllers/publications/dashboard/biblreview.js
+++ app/controllers/publications/dashboard/biblreview.js
@@ -5,7 +5,19 @@ export default Ember.Controller.extend({
     page: 1,
     queryParams: ['pubyear','pubtype','page', 'only_delayed'],
     needs: ['publications'],
-    only_delayed: false
+    only_delayed: false,
+    pubyears: Ember.computed(function(){      
+      return [
+        {pubyear: this.t('biblreview.selectPublicationYearPrompt'), id: 0},
+        {pubyear: moment().year()   + ' ' + this.t('biblreview.orLater'), id: 1},
+        {pubyear: moment().year()-1, id:moment().year()-1},
+        {pubyear: moment().year()-2, id:moment().year()-2},
+        {pubyear: moment().year()-3, id:moment().year()-3},
+        {pubyear: moment().year()-4, id:moment().year()-4},
+        {pubyear: moment().year()-5 + ' ' + this.t('biblreview.orEarlier'), id:-1}
+      ];
+    })
+
 });
 
 
--- app/routes/publications/dashboard/biblreview.js
+++ app/routes/publications/dashboard/biblreview.js
@@ -27,20 +27,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
   setupController: function(controller, models) {
     controller.set("model", models.publicationList);
-    controller.set("pubyears", []);
-    controller.get("pubyears").addObjects([
-      {pubyear: this.t('biblreview.selectPublicationYearPrompt'), id: 0},
-      {pubyear: moment().year()   + ' ' + this.t('biblreview.orLater'), id: 1},
-      {pubyear: moment().year()-1, id:moment().year()-1},
-      {pubyear: moment().year()-2, id:moment().year()-2},
-      {pubyear: moment().year()-3, id:moment().year()-3},
-      {pubyear: moment().year()-4, id:moment().year()-4},
-      {pubyear: moment().year()-5 + ' ' + this.t('biblreview.orEarlier'), id:-1}
-      ]);
   },
 	actions: {
 		editItem: function(item, params) {
-			console.log("transitionTo publication.show.edit", item, params);
 			this.transitionTo('publications.show.edit', item.id, {queryParams: params});
 		}
 	}
