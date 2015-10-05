d2ac9060d59a4d781e38bebbf3ad553d6de278c1
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-10-05 11:05:11 +0200

    Remove defaultLocale from index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel: function() {
-		var defaultLang = this.controllerFor("application").getDefaultLocale();
-		this.transitionTo('publications.dashboard.drafts', {queryParams: {lang: defaultLang}});
+		this.transitionTo('publications.dashboard.drafts');
 	}
 });
