8ca35e9afcb90356265cbe9b309956142093d6de
Magnus Kull <magnus.kull@ub.gu.se>
2015-12-10 16:59:39 +0100

    preview component for publication field

--- app/components/publication-display.js
+++ app/components/publication-display.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  isReviewMode: Ember.computed.equal('mode', 'review'),
+  isPreviewMode: Ember.computed.equal('mode', 'preview'),
 
   allFieldObjects: Ember.computed('publicationType.all_fields', function() {
     var o = Ember.Object.create();
@@ -10,5 +12,5 @@ export default Ember.Component.extend({
     });
     return o;
   })
-  
+
 });
