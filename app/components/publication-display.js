c0195919bdfda7aafa67fc0a7392bb18d9dc6d07
Magnus Kull <magnus.kull@ub.gu.se>
2015-12-10 18:09:15 +0100

    begin styling of publication display

--- app/components/publication-display.js
+++ app/components/publication-display.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  classNames: ['publication-display'],
+
+  classNameBindings: ['isReviewMode:publication-review-list', 'isPreviewMode:publication-preview-list'],
+
   isReviewMode: Ember.computed.equal('mode', 'review'),
   isPreviewMode: Ember.computed.equal('mode', 'preview'),
 
