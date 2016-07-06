bb9edebdfef57dad6a4f597cd23fa34223a6dad9
Per Larsson <per.larsson@ub.gu.se>
2016-07-06 16:31:05 +0200

    GUP-427 Fixed comma bug in author list.

--- app/components/authors-string.js
+++ app/components/authors-string.js
@@ -5,9 +5,16 @@ export default Ember.Component.extend({
   tagName: 'span',
   authorsString: Ember.computed('authors', function(){
     var string = '';
-    this.get('authors').forEach(function(author){
+    this.get('authors').forEach(function(author, index){
       var currentAuthorString = author.first_name + ' ' + author.last_name;
-      string += currentAuthorString + ' ';
+      if (index === 0)
+      {
+        string += currentAuthorString;
+      }
+      else
+      {
+        string += ", " + currentAuthorString;      	
+      }
     });
     return string;
   })
