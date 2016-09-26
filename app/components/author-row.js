84ba6958d5b0776699e84be4344c9107ad627570
David Gustafsson <david.gustafsson@ub.gu.se>
2016-09-26 14:56:07 +0200

    Normalize formatting

--- app/components/author-row.js
+++ app/components/author-row.js
@@ -1,25 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	errors: null,
-	didInsertElement: function() {
-	},
-	resetForm: function() {
-		if (this.get("item.newAuthorForm")) {
-			if(!this.get('item.importedAuthorName')) {
-				this.set("item.newAuthorForm.firstName", '');
-				this.set("item.newAuthorForm.lastName", '');
-			}
-			this.set("item.newAuthorForm.year_of_birth", '');
-			this.set("item.newAuthorForm.xaccount", '');
-			this.set("item.newAuthorForm.orcid", '');
-		}
-	},
+  errors: null,
+  didInsertElement: function() {
+  },
+  resetForm: function() {
+    if (this.get("item.newAuthorForm")) {
+      if(!this.get('item.importedAuthorName')) {
+        this.set("item.newAuthorForm.firstName", '');
+        this.set("item.newAuthorForm.lastName", '');
+      }
+      this.set("item.newAuthorForm.year_of_birth", '');
+      this.set("item.newAuthorForm.xaccount", '');
+      this.set("item.newAuthorForm.orcid", '');
+    }
+  },
 
-	// Used to signal select2-adjusted component to set a default query string
-	setDefaultQuery: Ember.computed('item.importedAuthorName', function() {
-		return !!this.get('item.importedAuthorName');
-	}),
+  // Used to signal select2-adjusted component to set a default query string
+  setDefaultQuery: Ember.computed('item.importedAuthorName', function() {
+    return !!this.get('item.importedAuthorName');
+  }),
 
   showInputFields: Ember.computed('item.importedAuthorName', 'addAffiliation', function(){
     return (!!this.get('item.importedAuthorName') && this.get('addAffiliation')) || !this.get('item.importedAuthorName');
@@ -29,59 +29,59 @@ export default Ember.Component.extend({
     return !!this.get('item.importedAuthorName') && !this.get('addAffiliation');
   }),
 
-	newAuhtorFormVisible: function() {
-		var self = this;
-		if (this.get("item.transformedToNewAuthor") === true) {
-			this.resetForm();
-			Ember.run.later(function() {
-				self.$().find('#first-name').focus();
-			});
-		}
- 	}.observes('item.transformedToNewAuthor'),
+  newAuhtorFormVisible: function() {
+    var self = this;
+    if (this.get("item.transformedToNewAuthor") === true) {
+      this.resetForm();
+      Ember.run.later(function() {
+        self.$().find('#first-name').focus();
+      });
+    }
+  }.observes('item.transformedToNewAuthor'),
 
-	actions: {
-		setMsgHeader: function(type, msg) {
-			this.sendAction('setMsgHeader', type, msg);
-		},
-	    queryAuthors: function(query, deferred) {
-	      deferred.reject = function(reason) {
-	        console.log(reason);
-	      };
-	      var success = function(data) {
-	      	data = data.map(function(item){
-	      	  // Create presentation string
-	      	  var nameStr = [item.first_name, item.last_name].compact().join(' ');
-	      	  var yearStr = [item.year_of_birth].compact().join('');
-			  var idStr = [item.xaccount, item.orcid].compact().join(', ')
+  actions: {
+    setMsgHeader: function(type, msg) {
+      this.sendAction('setMsgHeader', type, msg);
+    },
+    queryAuthors: function(query, deferred) {
+      deferred.reject = function(reason) {
+        console.log(reason);
+      };
+      var success = function(data) {
+        data = data.map(function(item){
+          // Create presentation string
+          var nameStr = [item.first_name, item.last_name].compact().join(' ');
+          var yearStr = [item.year_of_birth].compact().join('');
+          var idStr = [item.xaccount, item.orcid].compact().join(', ')
 
-			  if(yearStr) {
-			    yearStr = ', ' + yearStr
-			  }
-			  if(idStr) {
-			    idStr = ' ' + ['(', idStr, ')'].join('')
-			  }
-	      	  item.presentation_string = nameStr + yearStr + idStr;
-	      	  return item;
-	      	});
-	        return deferred.resolve(data);
-	      };
+            if(yearStr) {
+              yearStr = ', ' + yearStr
+            }
+          if(idStr) {
+            idStr = ' ' + ['(', idStr, ')'].join('')
+          }
+          item.presentation_string = nameStr + yearStr + idStr;
+          return item;
+        });
+        return deferred.resolve(data);
+      };
 
-	      var fromStore = this.store.find("person_record", {search_term: query.term});
-	      fromStore.then(success, deferred.reject);
+      var fromStore = this.store.find("person_record", {search_term: query.term});
+      fromStore.then(success, deferred.reject);
 
-	    },
+    },
 
-	    moveUpOne: function(id) {
-	    	this.sendAction('moveUp', id);
-	    },
+    moveUpOne: function(id) {
+      this.sendAction('moveUp', id);
+    },
 
-	    moveDownOne: function(id) {
-	    	this.sendAction('moveDown', id);
-	    },
+    moveDownOne: function(id) {
+      this.sendAction('moveDown', id);
+    },
 
-	    remove: function(id) {
-	    	this.sendAction('removeAuthor', id);
-	    },
+    remove: function(id) {
+      this.sendAction('removeAuthor', id);
+    },
 
     toggleAddAffiliation: function(){
       var that = this;
@@ -92,35 +92,35 @@ export default Ember.Component.extend({
         obj.select2('open');
       });
     },
-	    toggleAddNewAuthor: function(item) {
-			if (item.get("transformedToNewAuthor") === true) {
-				item.set("transformedToNewAuthor", false);
-			}
-			else {
-				item.set("transformedToNewAuthor", true);
-			}
+    toggleAddNewAuthor: function(item) {
+      if (item.get("transformedToNewAuthor") === true) {
+        item.set("transformedToNewAuthor", false);
+      }
+      else {
+        item.set("transformedToNewAuthor", true);
+      }
 
-	    },
-	    createAuthor: function(item) {
-	    	var that = this;
-	        var successHandler = function(model) {
-	            item.set('selectedAuthor', model);
-	            item.set('transformedToNewAuthor', false);
-	        };
-	        var errorHandler = function(reason) {
-	        	that.send('setMsgHeader', 'error', reason.error.msg);
-				that.set('errors', reason.error.errors);
-	            Ember.run.later(function() {
-	                Ember.$('[data-toggle="popover"]').popover({
-	                    placement: 'top',
-	                    html: true
-	                });
-	            });
-	        };
-	        //console.log(newAuthor.get('firstName'));
-	        this.store.save('person',{'first_name': item.newAuthorForm.get('firstName'), 'last_name': item.newAuthorForm.get('lastName'), 'year_of_birth': item.newAuthorForm.get('year_of_birth'),
-	                            'xaccount': item.newAuthorForm.get('xaccount'), 'orcid': item.newAuthorForm.get('orcid') }).then(successHandler, errorHandler);
-	    },
+    },
+    createAuthor: function(item) {
+      var that = this;
+      var successHandler = function(model) {
+        item.set('selectedAuthor', model);
+        item.set('transformedToNewAuthor', false);
+      };
+      var errorHandler = function(reason) {
+        that.send('setMsgHeader', 'error', reason.error.msg);
+        that.set('errors', reason.error.errors);
+        Ember.run.later(function() {
+          Ember.$('[data-toggle="popover"]').popover({
+            placement: 'top',
+            html: true
+          });
+        });
+      };
+      //console.log(newAuthor.get('firstName'));
+      this.store.save('person',{'first_name': item.newAuthorForm.get('firstName'), 'last_name': item.newAuthorForm.get('lastName'), 'year_of_birth': item.newAuthorForm.get('year_of_birth'),
+        'xaccount': item.newAuthorForm.get('xaccount'), 'orcid': item.newAuthorForm.get('orcid') }).then(successHandler, errorHandler);
+    },
     addInstitution: function(institution){
       // Add institution to selected array
       var institutionObject = Ember.Object.create(institution);
@@ -131,5 +131,5 @@ export default Ember.Component.extend({
       institutionsElement.addObject(institutionObject);
       Ember.$(id).select2('data', institutionsElement);
     }
-	}
+  }
 });
