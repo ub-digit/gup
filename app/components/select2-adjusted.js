bab56776b4ca1738ec660997de862dede870ef80
David Gustafsson <david.gustafsson@ub.gu.se>
2016-09-27 18:51:23 +0200

    Avoid autoRun, simplify and optimize

--- app/components/select2-adjusted.js
+++ app/components/select2-adjusted.js
@@ -1,18 +1,16 @@
 import Ember from 'ember';
 import Select2 from 'ember-select-2/components/select-2';
 
-var get = Ember.get;
-var run = Ember.run;
 export default Select2.extend({
   didInsertElement: function() {
     this._super(...arguments);
-    var self = this;
-    Ember.run.later(function() {
-      Ember.$("." + self.get('cssClass')).find("#toggleBtn").bind('click', function() {
-        self.set("zeroResult", true);
-        // close dropdown
+    var selector = '.' + this.get('cssClass');
+    Ember.$(selector).find('.select2-drop').append('<div class="select2-footer"><p>' + this.get('didNotFindWhatYouWereLookingForStr') + '</p><button id="toggleBtn" class="btn btn-primary">'+ this.get('btnText') + '</button></div>');
+    Ember.$(selector).find("#toggleBtn").bind('click', () => {
+      Ember.run(() => {
+        // Close dropdown
+        this.set('zeroResult', true);
       });
     });
-    Ember.$("." + self.get('cssClass')).find('.select2-drop').append("<div class='select2-footer'><p>" + this.get("didNotFindWhatYouWereLookingForStr") + "</p><button id='toggleBtn' class='btn btn-primary'>"+ this.get("btnText") + "</button></div>");
   },
 });
