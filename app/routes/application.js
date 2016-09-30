747ac6a5ac53950b9a7b6b4aad0af70082892f2c
David Gustafsson <david.gustafsson@ub.gu.se>
2016-09-30 14:21:33 +0200

    Annotate seemingly unused import

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin'; //Remove?
 
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
