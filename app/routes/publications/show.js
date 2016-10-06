5ddc142e2ab9fdab2560f9c4f7cc886cec396911
David Gustafsson <david.gustafsson@ub.gu.se>
2016-10-06 14:20:23 +0200

    Normalize indentation

--- app/routes/publications/show.js
+++ app/routes/publications/show.js
@@ -2,22 +2,21 @@ import Ember from 'ember';
 import ResetScroll from 'gup/mixins/resetscroll';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
 export default Ember.Route.extend(AuthenticatedRouteMixin,ResetScroll, {
   i18n: Ember.inject.service(),
 
-
   beforeModel: function() {
-		Ember.$("body").addClass("loading");
-	},
-
-  model: function(params){
-    return this.store.find('publication',params.id);
+    // TODO: loading substate instead (below should not work?)
+    Ember.$('body').addClass('loading');
   },
 
-	afterModel: function(/*model, transition */) {
-		Ember.$("body").removeClass("loading");
-	},
+  model: function(params) {
+    return this.store.find('publication', params.id);
+  },
+
+  afterModel: function(/*model, transition */) {
+    Ember.$('body').removeClass('loading');
+  },
 
   actions: {
     refreshModel: function(modelId) {
@@ -25,7 +24,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,ResetScroll, {
     },
     error: function() {
       this.send('setMsgHeader', 'error', this.get('i18n').t('messages.publicationNotFound'));
-	  this.transitionTo('publications.dashboard.start');
+      this.transitionTo('publications.dashboard.start');
     }
   }
 });
