caa6fad738473f3b725e7a011211edff2941e1c9
David Gustafsson <david.gustafsson@ub.gu.se>
2016-11-30 17:53:38 +0100

    Fix JSLint issues in routes/admin.js

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,21 +1,20 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-	i18n: Ember.inject.service(),
-	titleToken: function() {
-//	  return this.get("i18n").t('admin.title');
-	},
-	model: function(params) {
-		return Ember.RSVP.hash({
-		  faculties: this.store.find('faculty')
-		});
-	},
+  i18n: Ember.inject.service(),
+  titleToken: function() {
+    //return this.get("i18n").t('admin.title');
+  },
+  model: function() {
+    return Ember.RSVP.hash({
+      faculties: this.store.find('faculty')
+    });
+  },
 
-	setupController: function(controller, model) {
-		this._super(...arguments);
-		controller.set("faculties", model.faculties);
-	},
+  setupController: function(controller, model) {
+    this._super(...arguments);
+    controller.set("faculties", model.faculties);
+  },
 
 });
