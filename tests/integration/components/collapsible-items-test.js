f2b9378c6673ee8926047ae764d28b3394f45037
David Gustafsson <david.gustafsson@ub.gu.se>
2016-12-01 15:29:51 +0100

    Write basic integration tests for collapsibe-items component

--- tests/integration/components/collapsible-items-test.js
+++ tests/integration/components/collapsible-items-test.js
@@ -8,17 +8,30 @@ moduleForComponent('collapsible-items', 'Integration | Component | collapsible i
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{collapsible-items}}`);
-
   assert.equal(this.$().text().trim(), '');
+});
 
+test('it works', function(assert) {
+  this.set('items', ['Item 1', 'Item 2']);
   // Template block usage:
   this.render(hbs`
-    {{#collapsible-items}}
-      template block text
+    {{#collapsible-items items=items as |item collapsibleItem|}}
+      <button class="toggle-item-{{collapsibleItem.index}}" {{action collapsibleItem.toggleIsActive}}>Toggle text</button>
+      {{#if collapsibleItem.isActive}}{{item}}{{/if}}
     {{/collapsible-items}}
   `);
+  // Should not display any items
+  assert.equal(this.$(":contains('Item')").length, 0, "All items collapsed");
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // Toggle visible
+  this.$('.toggle-item-0').click();
+  assert.equal(this.$(":contains('Item 11')").length, 1, "First item expanded");
+
+  // Toggle hidden
+  this.$('.toggle-item-0').click();
+  assert.equal(this.$(":contains('Item 1')").length, 0, "First item collapsed");
+
+  //TODO: Test accordion behavior
 });
+
