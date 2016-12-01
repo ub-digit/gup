7b34b721bd6244424f681701f051f39f46153f0f
David Gustafsson <david.gustafsson@ub.gu.se>
2016-12-01 19:04:11 +0100

    Add partly non-working test for confirmation-modal, non working bits commented out

--- tests/integration/components/confirmation-modal-test.js
+++ tests/integration/components/confirmation-modal-test.js
@@ -1,24 +1,45 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import Ember from 'ember';
 
 moduleForComponent('confirmation-modal', 'Integration | Component | confirmation modal', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it works', function(assert) {
+  assert.expect(4);
 
-  this.render(hbs`{{confirmation-modal}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+  this.set('isShowingModal', false);
+  this.set('didConfirmAction', () => {
+    this.set('didConfirmActionCalled', true);
+    return Ember.RSVP.Promise.resolve();
+  });
   this.render(hbs`
-    {{#confirmation-modal}}
+    {{#confirmation-modal isShowing=isShowingModal didConfirm=(action didConfirmAction)}}
       template block text
     {{/confirmation-modal}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$('.modal').is(':hidden'), 'Modal is not visible if not showing');
+
+  this.set('isShowingModal', true);
+
+  //Wait for modal animation to complete
+  return wait().then(() => {
+    // @TODO: GET THIS WORKING, problem with wait????
+    //assert.ok(this.$('.modal').is(':visible'), 'Modal is visible if showing');
+
+    assert.equal(this.$('.modal-body').text().trim(), 'template block text', 'Modal body contains block text');
+
+    // @TODO: .btn-primary is a bit fragile, but ok. Add classes in component?
+    this.$('.btn-primary').click();
+    assert.ok(this.get('didConfirmActionCalled'), 'didConfirm action was called when clicking acccept button');
+
+    this.$('.close').click();
+    return wait().then(() => {
+      assert.ok(this.$('.modal').is(':hidden'), 'Modal is not visible after clicking close button');
+    });
+    //TODO: also click cancel button?
+  });
 });
