ed1b339e586e9297f853ab800ff818d41a385a40
David Gustafsson <david.gustafsson@ub.gu.se>
2016-12-01 14:53:26 +0100

    Remove old broken test

--- tests/unit/views/publications/dashboard/manage/search-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('view:publications/dashboard/manage/search', 'Unit | View | publications/dashboard/manage/search');
-
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var view = this.subject();
-  assert.ok(view);
-});
