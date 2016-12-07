2f6631a7722e643ebfc498adf54770e39ab35487
Per Larsson <per.larsson@ub.gu.se>
2016-12-07 15:13:54 +0100

    Added query params to publication lists.

--- app/controllers/publications_list.js
+++ app/controllers/publications_list.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	i18n: Ember.inject.service(),
   	page: 1,
-  	queryParams: ['page', 'sort_by'],
+  	queryParams: ['page', 'sort_by', 'person_id', 'department_id', 'faculty_id', 'serie_id', 'publication_type', 'ref_value', 'start_year', 'end_year'],
   	sortSelectValues: Ember.A([]), 
-  	sort_by: 'pubyear'
+  	sort_by: 'pubyear',
+    person_id: null,
+    department_id: null,
+    faculty_id: null,
+    serie_id: null,
+    publication_type: null,
+    ref_value: null,
+    start_year: null,
+    end_year: null
 });
-
-
-
