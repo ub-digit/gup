3d6f44fd6f408c5ea23f3aa1d8601215de244b0f
johan larsson <johan.larsson@ub.gu.se>
2016-12-16 14:46:52 +0100

    replacewith used instead of transitionto

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel: function() {
-		this.transitionTo('publications.dashboard.manage.published');
+		this.replaceWith('publications.dashboard.manage.published');
 	}
 });
