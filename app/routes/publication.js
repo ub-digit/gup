69f91442c2ce53ccbd96d2371657a0b61a8825e4
Per Larsson <per.larsson@ub.gu.se>
2018-06-26 11:45:09 +0200

    Add meta and link tags in head

--- app/routes/publication.js
+++ app/routes/publication.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Route.extend({
   titleToken: function(model) {
@@ -9,6 +10,10 @@ export default Ember.Route.extend({
       publicationTypes: this.store.find('publication_type'),
       publication: this.store.find('publication', params.id)
     });
+  },
+
+  afterModel: function(models) {
+    this.setHeadTags(models);
   },
 
   setupController: function(controller, models) {
@@ -23,5 +28,36 @@ export default Ember.Route.extend({
       controller.set('publication', null);
       controller.set('selectedPublicationType', null);
     }
+  },
+
+  setHeadTags: function(models) {
+    var headTags = [];
+
+    headTags.push({type: 'link', attrs: {name: 'canonical', content: ENV.APP.publicationURL + '/' + models.publication.id}});
+    if (models.publication.keywords) {
+      headTags.push({type: 'meta', attrs: {name: 'keywords', content: models.publication.keywords}});
+    }
+    headTags.push({type: 'meta', attrs: {name: 'citation_title', content: models.publication.title}});
+    headTags.push({type: 'meta', attrs: {name: 'citation_publication_date', content: models.publication.pubyear}});
+    if (models.publication.abstract) {
+      headTags.push({type: 'meta', attrs: {name: 'citation_abstract', content: models.publication.abstract}});
+    }
+    if (models.publication.publanguage) {
+      headTags.push({type: 'meta', attrs: {name: 'citation_language', content: models.publication.publanguage}});
+    }
+    models.publication.publication_identifiers.forEach(function (identifierObj) {
+      if (identifierObj.identifier_code === 'doi') {
+        headTags.push({type: 'meta', attrs: {name: 'citation_doi', content: identifierObj.identifier_value}});
+      }
+    });
+    models.publication.authors.forEach(function (authorObj) {
+      var authorName = [authorObj.first_name, authorObj.last_name].join(" ").trim();
+      headTags.push({type: 'meta', attrs: {name: 'citation_author', content: authorName}});
+    });
+    models.publication.files.forEach(function (fileObj) {
+      headTags.push({type: 'meta', attrs: {name: 'citation_pdf_url', content: ENV.APP.fileURL + '/'+ fileObj.id}});
+    });
+
+    this.set('headTags', headTags);
   }
 });
