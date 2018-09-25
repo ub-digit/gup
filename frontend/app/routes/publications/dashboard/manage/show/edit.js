8395941c7ec73cc8b4d109b3f66953de2f836ac0
Per Larsson <per.larsson@ub.gu.se>
2018-09-25 09:50:02 +0200

    Modifications for merging gup and gup-server

--- frontend/app/routes/publications/dashboard/manage/show/edit.js
+++ frontend/app/routes/publications/dashboard/manage/show/edit.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import ResetScroll from 'gup/mixins/resetscroll';
-
-//import ENV from 'gup/config/environment';
+import ResetScroll from 'frontend/mixins/resetscroll';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, ResetScroll, {
   i18n: Ember.inject.service(),
