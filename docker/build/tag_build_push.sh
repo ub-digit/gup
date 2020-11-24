b16a48851885ddbd3b272457917b7f3f116e373a
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2020-11-24 13:33:14 +0100

    Check if first parameter was provided

--- docker/build/tag_build_push.sh
+++ docker/build/tag_build_push.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if test "$1" = ""
+then
+  echo Usage: $0 git-revision-tag
+  exit
+fi
+
+# ./tag_build_push.sh xljoha-release-2021.01.001
+
 git tag $1
 git push origin $1
 GIT_REVISION=$1 docker-compose build
