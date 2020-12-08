426344c020e4725c6b74f8213afeba3fc2080b62
David Gustafsson <glasklas@gmail.com>
2020-12-08 16:56:14 +0100

    Use env instead of file for auth override

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,18 +1,16 @@
 # Read config files and store applicable values in APP_CONFIG constant
 main_config = YAML.load_file("#{Rails.root}/config/config.yml")
-data_sources_config = YAML.load_file("#{Rails.root}/config/data_sources.yml")
 languages_config = YAML.load_file("#{Rails.root}/config/languages.yml")
 publication_identifier_codes_config = YAML.load_file("#{Rails.root}/config/publication_identifier_codes.yml")
-if Rails.env == 'test'
-  secret_config = YAML.load_file("#{Rails.root}/config/config_secret.test.yml")
-  data_sources_config = YAML.load_file("#{Rails.root}/config/data_sources_test.yml")
-else
-  # TODO: Hack, enable erb for all configs or set in config.rb?
-  require "erb"
-  require "yaml"
-  yaml = Pathname.new("#{Rails.root}/config/config_secret.yml")
-  secret_config = YAML.load(ERB.new(yaml.read).result(binding))
-end
+
+data_sources_config_file = Rails.env == 'test' ? 'config/data_sources_test.yml' : 'config/data_sources.yml'
+data_sources_config = YAML.load_file("#{Rails.root}/#{data_sources_config_file}")
+
+secret_config_file = Rails.env == 'test' ? 'config/config_secret.test.yml' : 'config/config_secret.yml'
+require "erb"
+require "yaml"
+yaml = Pathname.new("#{Rails.root}/#{secret_config_file}")
+secret_config = YAML.load(ERB.new(yaml.read).result(binding))
 
 config = main_config
 config = config.merge(secret_config)
