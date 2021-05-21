e87157be96f1c07a84450e21a386342e978e52eb
Johan <johan.larsson@ub.gu.se>
2021-05-21 13:40:31 +0200

    adds settings for smtp

--- app/mailers/publication_mailer.rb
+++ app/mailers/publication_mailer.rb
@@ -7,11 +7,14 @@ class PublicationMailer < ActionMailer::Base
     @from = from
     @publication_id = publication_id
     subject = "GUP- Meddelande om pubid: #{publication_id}"
-    delivery_options = {address: APP_CONFIG['mail_settings']['delivery_options']['address'], 
+    delivery_options = {address: APP_CONFIG['mail_settings']['delivery_options']['address'],
                         port: APP_CONFIG['mail_settings']['delivery_options']['port'],
-                        enable_starttls_auto: false}
+                        enable_starttls_auto: APP_CONFIG['mail_settings']['delivery_options']['enable_starttls_auto'],
+                        user_name: APP_CONFIG['mail_settings']['delivery_options']['user_name'],
+                        password: APP_CONFIG['mail_settings']['delivery_options']['password'],
+                        authentication: APP_CONFIG['mail_settings']['delivery_options']['authentication']}
 
-    mail(subject: subject, 
+    mail(subject: subject,
           delivery_method_options: delivery_options
         )
   end
