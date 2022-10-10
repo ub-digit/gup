e937f784a791adfde464093799e8b90bb5a8414a
Per Larsson <per.larsson@ub.gu.se>
2022-10-10 14:19:22 +0200

    GUP-1228: Add artwork type values in english

--- app/controllers/v1/artwork_types_controller.rb
+++ app/controllers/v1/artwork_types_controller.rb
@@ -1,11 +1,22 @@
 class V1::ArtworkTypesController < V1::V1Controller
   api :GET, '/artwork_types', 'Returns a list of all type of works (for artistic works).'
   def index
-    types = ["Konsert, framträdanden, workshops", "Publicerad musik och ljudkonst", "Film, video och radio", "Utställningar, events och festivaler", "Fysiska artefakter", "Digitala artefakter"]
+    types =
+    [
+      "Concert, performances, workshops",
+      "Published music, sound art",
+      "Film, video, radio",
+      "Exhibitions, events festivals",
+      "Physical artifacts",
+      "Digital artifacts",
+      "Konsert, framträdanden, workshops",
+      "Publicerad musik och ljudkonst",
+      "Film, video och radio",
+      "Utställningar, events och festivaler",
+      "Fysiska artefakter",
+      "Digitala artefakter"
+    ]
     @response[:artwork_types] = types.map{|type| {label: type, value: type}}
     render_json
   end
 end
-
-
-
