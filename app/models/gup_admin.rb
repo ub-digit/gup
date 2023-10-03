26e8f417e5822db424ad04b5b99ba5b21f4900b5
Per Larsson <per.larsson@ub.gu.se>
2023-10-03 14:35:50 +0200

    Gup Admin enable flag is handled as a boolan

--- app/models/gup_admin.rb
+++ app/models/gup_admin.rb
@@ -12,14 +12,14 @@ class GupAdmin
     rows = ActiveRecord::Base.connection.exec_query(sql).rows
   end
   def self.put_to_index publication_id
-    should_abort = !APP_CONFIG.key?('gup_admin_settings') || !APP_CONFIG['gup_admin_settings']['enable'].eql?('true')
+    should_abort = !APP_CONFIG.key?('gup_admin_settings') || !APP_CONFIG['gup_admin_settings']['enable']
     return if should_abort
     document = GupAdmin.get_document publication_id
     RestClient.put "#{APP_CONFIG['gup_admin_settings']['base_url']}/publications/?api_key=#{APP_CONFIG['gup_admin_settings']['api_key']}", JSON.parse('{"data":' + document + '}').to_json ,  content_type: :json
   end
 
   def self.delete_from_index publication_id
-    should_abort = !APP_CONFIG.key?('gup_admin_settings') || !APP_CONFIG['gup_admin_settings']['enable'].eql?('true')
+    should_abort = !APP_CONFIG.key?('gup_admin_settings') || !APP_CONFIG['gup_admin_settings']['enable']
     return if should_abort
     RestClient.delete "#{APP_CONFIG['gup_admin_settings']['base_url']}/publications/gup_#{publication_id}?api_key=#{APP_CONFIG['gup_admin_settings']['api_key']}"
   end
