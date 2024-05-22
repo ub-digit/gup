0da85f29798b73b207f72c605b8d9f63111992a9
Johan Larsson <johan.larsson@ub.gu.se>
2024-05-22 11:11:48 +0200

    Update translations.js

--- frontend/app/locales/en/translations.js
+++ frontend/app/locales/en/translations.js
@@ -492,7 +492,7 @@ export default {
           publicationLinks: 'Links should begin with http:// or https://',
           authors: {
             helptext: {
-              general: 'Please register all authors in the same order as mentioned in the original publication. If the author is affiliated to GU, register family name and first name and if the author is not affiliated to GU, register family name and first name’s initial. Please search for already registered authors before creating a new one.',
+              general: 'Please register all authors in the same order as mentioned in the original publication. If the author is affiliated to GU, register family name and first name and the affiliation as it’s indicated in the publication. For other authors, register family name and first name’s initial. Please search for already registered authors before creating a new one.',
               artistic: 'Please register all authors. If the author is affiliated to GU, register family name and first name and if the author is not affiliated to GU, register family name and first name’s initial. Please search for already registered authors before creating a new one.',
             },
           },
