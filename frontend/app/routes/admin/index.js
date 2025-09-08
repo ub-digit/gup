c088f781f51e4c9ee525ba8ba2a034758ce83835
Per Larsson <per.larsson@ub.gu.se>
2025-09-08 13:04:05 +0200

    Change admin route transition from removed people to messages

--- frontend/app/routes/admin/index.js
+++ frontend/app/routes/admin/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    this.transitionTo('admin.people');
+    this.transitionTo('admin.messages');
   },
 
 });
